4a434d540db47303b0ba5cc1e56e2b8f5556ca89
cartok76 <damienhamel@hotmail.com>
2017-01-10 16:23:02 +0100

    Formulaire

--- app/Controller/ArticlesController.php
+++ app/Controller/ArticlesController.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dell
- * Date: 10/01/2017
- * Time: 14:38
- */
+
 namespace Controller;
 
 use \W\Controller\Controller;
---
+++ app/Controller/UsersController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 10/01/2017
+ * Time: 15:27
+ */
